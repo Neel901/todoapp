aec1455feb4832411d93133809b559587afe402f
Neel901 <chandranilotpal21@gmail.com>
2024-09-28 20:21:37 +0530

    added crossOrigin

--- src/main/java/com/example/todo/controller/AppController.java
+++ src/main/java/com/example/todo/controller/AppController.java
@@ -14,6 +14,7 @@ import com.example.todo.response.UpdateTaskResponse;
 import com.example.todo.service.AppService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class AppController {
 
     @Autowired
