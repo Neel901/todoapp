56a13a2d94fcdd7f3c577bf906b16486eedf6202
Neel901 <chandranilotpal21@gmail.com>
2024-09-29 23:09:14 +0530

    fix jpa query

--- todo/src/main/java/com/example/todo/repository/TaskRepository.java
+++ todo/src/main/java/com/example/todo/repository/TaskRepository.java
@@ -4,6 +4,7 @@ import com.example.todo.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,7 +16,7 @@ public interface TaskRepository extends JpaRepository<Task, Integer> {
     @Query("Update Task t set t.done = :isDone  where t.id = :taskId")
     @Modifying
     @Transactional
-    void updateTask(int taskId, Boolean isDone);
+    void updateTask(@Param("taskId") int taskId, @Param("isDone") Boolean isDone);
 
     List<Task> findAllByUserId(int userId);
 }
